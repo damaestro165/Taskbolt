bd41aa70050565d4efa3971df554ac061edd3090
damaestro165 <damaestro165@gmail.com>
2023-04-02 20:07:26 +0100

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { Box } from '@chakra-ui/react';
+import './App.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Navigate,
+  Outlet,
+} from 'react-router-dom';
+import Signup from './component/Signup';
+
+function App() {
+  const Root = () => {
+    return (
+      <Box className='container h-screen'>
+        <Outlet />
+      </Box>
+    );
+  };
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root />}>
+        <Route index element={<Signup />} />
+        <Route path='/login' element={<div>Login</div>} />
+        <Route path='*' element={<Navigate to='/' />} />
+      </Route>
+    )
+  );
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Signup.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div className='flex flex-col md:flex-row h-screen'>
+      <div className='bg-[#5720DD] md:h-screen h-1/3  md:w-2/6  w-full'>
+        hollo
+      </div>
+      <div className='md:w-2/6 bg-white h-2/3 md:h-screen'> good</div>
+    </div>
+  );
+};
+
+export default Signup;
