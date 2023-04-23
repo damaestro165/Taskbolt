c95558f54b1eeb39351f062bc29cb02105e7c479
damaestro165 <damaestro165@gmail.com>
2023-04-23 22:21:41 +0100

    chore: temporary removed protected route

--- src/component/ProtectedRoute.jsx
+++ src/component/ProtectedRoute.jsx
@@ -1,14 +1,15 @@
 import { Navigate, Outlet } from 'react-router-dom';
 const ProtectedRoute = () => {
-  const userData = localStorage.getItem('User');
-  let token = null;
+  // const userData = localStorage.getItem('User');
+  // let token = null;
 
-  if (userData) {
-    const { token: userToken } = JSON.parse(userData);
-    token = userToken;
-  }
+  // if (userData) {
+  //   const { token: userToken } = JSON.parse(userData);
+  //   token = userToken;
+  // }
 
-  return token !== null ? <Outlet /> : <Navigate to='/login' />;
+  // return token !== null ? <Outlet /> : <Navigate to='/login' />;
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
